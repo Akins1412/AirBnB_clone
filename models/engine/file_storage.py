6a1cfda63619dd0c1fb88743b50c2f665205efeb
Emmanuel <138694714+Akins1412@users.noreply.github.com>
2024-03-10 12:51:02 +0000

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This! module stores instances from the Baseclass"""
 
-from datetime import datetime
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -26,37 +25,26 @@ class FileStorage:
     def new(self, obj):
         """ Sets the `obj` with the `key` in `__objects`.
         """
-        cls_name = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[cls_name] = obj
+        cls_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(cls_name, obj.id)] = obj
 
     def save(self):
         """ Serialize `__objects` to file in  JSON
         """
-        serialized_objects = {}
-        for ke, value in FileStorage.__objects.items():
-            serialized_objects[ky] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(serialized_objects, f)
+        objdict = FileStorage.__objects
+        o_dict = {obj: objdict[obj].to_dict() for ob in objdict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(o_dict, f)
 
     def reload(self):
         """ If the JSON file exists, deserializes the file to `__objects`
         """
         try:
-            with open(FileStorage.__file_path, 'r') as f:
-                data = json.load(f)
-                for key, val in data.items():
-
-                    # Remove from the dictionary
-                    del val["__class__"]
-
-                    # Ensure conversion to datetime object
-                    form = "%Y-%m-%dT%H:%M:%S.%f"
-                    cl = "updated_at"  # Attribute to match
-                    if cl in val:
-                        val[cl] = datetime.strptime(value[cl], form)
-
-                    cls_name, obj_id = key.split('.')
-                    obj_instance = eval(class_name)(**value)
-		FileStorage.__objects[key] = obj_instance
+             with open(FileStorage.__file_path) as f:
+                o_jdict = json.load(f)
+                for ob in o_dict.values():
+                    cls_name = ob["__class__"]
+                    del ob["__class__"]
+                    self.new(eval(name_cls)(**ob))
         except FileNotFoundError:
             pass
